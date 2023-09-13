e2846328a73f86db349fb8b4f47cff093a1ee205
GarethG <ggomersall@gmail.com>
2023-09-13 12:55:36 +0100

    Added qrcode functionality

--- books/models.py
+++ books/models.py
@@ -3,6 +3,12 @@ from publishers.models import Publisher
 from authors.models import Author
 from django.utils.text import slugify
 import uuid
+#imports for qrcode generation
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image
+
 
 # Create your models here.
 """
@@ -42,8 +48,15 @@ class Book(models.Model):
       if not self.book_id:
         self.book_id = str(uuid.uuid4()).replace("-", "")[:24].lower()
 
-      #generate qr code
-
+        #generate qr code
+        qrcode_img = qrcode.make(self.book_id)
+        canvas = Image.new('RGB', (qrcode_img.pixel_size, qrcode_img.pixel_size), 'white')
+        canvas.paste(qrcode_img)
+        fname = f'qr_code-{self.title}.png'
+        buffer = BytesIO()
+        canvas.save(buffer, 'PNG')
+        self.qr_code.save(fname, File(buffer), save=False)
+        canvas.close()
 
       super().save(*args, **kwargs)
   
